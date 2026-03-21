e00aafbd42df7c0179823d0819b1de5a1b58f3db
Alena Ageeva <alenaageevapv@gmail.com>
2026-03-21 20:36:49 -0300

    feat: introduce UserEntity

---
+++ src/main/java/ru/ageeva/userservice/entity/UserEntity.java
@@ -0,0 +1,28 @@
+package ru.ageeva.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usera")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+}
